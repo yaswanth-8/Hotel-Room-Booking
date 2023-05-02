0353be60ddaacc81f8e3d6e31163ed736239d9b7
yaswanth <ya8wanth@gmail.com>
2023-05-02 21:14:30 +0530

    GET and POST API for Hotel

---
+++ Controllers/HotelApiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hotel_Room_Booking.Data;
+using Hotel_Room_Booking.Models;
+
+namespace Hotel_Room_Booking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HotelApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/HotelApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HotelModel>>> GetHotelModel()
+        {
+          if (_context.HotelModel == null)
+          {
+              return NotFound();
+          }
+            return await _context.HotelModel.ToListAsync();
+        }
+
+        // GET: api/HotelApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HotelModel>> GetHotelModel(int id)
+        {
+          if (_context.HotelModel == null)
+          {
+              return NotFound();
+          }
+            var hotelModel = await _context.HotelModel.FindAsync(id);
+
+            if (hotelModel == null)
+            {
+                return NotFound();
+            }
+
+            return hotelModel;
+        }
+
+        // PUT: api/HotelApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotelModel(int id, HotelModel hotelModel)
+        {
+            if (id != hotelModel.HotelId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hotelModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HotelApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<HotelModel>> PostHotelModel(HotelModel hotelModel)
+        {
+          if (_context.HotelModel == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.HotelModel'  is null.");
+          }
+            _context.HotelModel.Add(hotelModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHotelModel", new { id = hotelModel.HotelId }, hotelModel);
+        }
+
+        // DELETE: api/HotelApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotelModel(int id)
+        {
+            if (_context.HotelModel == null)
+            {
+                return NotFound();
+            }
+            var hotelModel = await _context.HotelModel.FindAsync(id);
+            if (hotelModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.HotelModel.Remove(hotelModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool HotelModelExists(int id)
+        {
+            return (_context.HotelModel?.Any(e => e.HotelId == id)).GetValueOrDefault();
+        }
+    }
+}
--- Controllers/HotelModelsController.cs
+++ Controllers/HotelModelsController.cs
@@ -7,22 +7,34 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Hotel_Room_Booking.Data;
 using Hotel_Room_Booking.Models;
+using Newtonsoft.Json;
 
 namespace Hotel_Room_Booking.Controllers
 {
     public class HotelModelsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        Uri baseAddress = new Uri("https://localhost:7258/api");
+        HttpClient client;
 
         public HotelModelsController(ApplicationDbContext context)
         {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
             _context = context;
         }
 
         // GET: HotelModels
         public async Task<IActionResult> Index(string searchCity)
         {
-            var hotels = await _context.HotelModel.ToListAsync();
+            List<HotelModel> hotels = new List<HotelModel>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/HotelApi").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                hotels = JsonConvert.DeserializeObject<List<HotelModel>>(data);
+            }
+           
             var cities = new SelectList(hotels.Select(h => h.City).Distinct().OrderBy(c => c));
 
             ViewBag.Cities = cities;
@@ -32,8 +44,8 @@ namespace Hotel_Room_Booking.Controllers
                 return View(hotels);
             }
 
-            return _context.HotelModel != null ? 
-                          View(await _context.HotelModel.ToListAsync()) :
+            return hotels != null ? 
+                          View( hotels) :
                           Problem("Entity set 'ApplicationDbContext.HotelModel'  is null.");
         }
 
@@ -68,10 +80,12 @@ namespace Hotel_Room_Booking.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("HotelId,City,Address,HotelName,Rooms,Image,Description,Price")] HotelModel hotelModel)
         {
-            if (ModelState.IsValid)
+            string data = JsonConvert.SerializeObject(hotelModel);
+            StringContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(client.BaseAddress + "/HotelApi", content).Result;
+
+            if (response.IsSuccessStatusCode)
             {
-                _context.Add(hotelModel);
-                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(hotelModel);
